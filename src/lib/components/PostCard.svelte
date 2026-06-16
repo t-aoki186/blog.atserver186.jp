959c3aa0af91b31f115886df0e1f4d5f50acf0c9
taoki <84th.tccaoki@gmail.com>
2026-06-16 22:35:39 +0900

    feat: 記事カードにカテゴリ表示を追加

--- src/lib/components/PostCard.svelte
+++ src/lib/components/PostCard.svelte
@@ -13,10 +13,13 @@
 				<img src={post.thumbnail} alt="{post.title}のサムネイル" class="w-30 rounded-lg" />
 				<div class="flex-1">
 					<h2 class="mb-2 text-2xl">{post.title}</h2>
+					<p>{post.heading}</p>
 					<div class="flex">
-						<p>{post.heading}</p>
 						<div class="ml-auto w-fit rounded-2xl border border-gray-500 px-2 py-1 text-right">
-							<p class="text-xs">{post.author}</p>
+							<p class="text-xs"><i class="fa-solid fa-folder-open mr-1"></i>{post.category}</p>
+						</div>
+						<div class="ml-2 w-fit rounded-2xl border border-gray-500 px-2 py-1 text-right">
+							<p class="text-xs"><i class="fa-regular fa-circle-user mr-1"></i>{post.author}</p>
 						</div>
 					</div>
 				</div>
