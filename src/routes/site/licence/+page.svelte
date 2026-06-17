ffc8644808cd35e3976a57f238f30a2d8a133081
taoki <84th.tccaoki@gmail.com>
2026-06-17 22:47:12 +0900

    add: ライセンスページにnodeモジュール以外の素材のライセンス表記を追加

--- src/routes/site/licence/+page.svelte
+++ src/routes/site/licence/+page.svelte
@@ -17,10 +17,56 @@
 </div>
 
 <div class="mt-4">
-	<p>生成日時: {data.generatedAt}</p>
-	<ul>
-		{#each data.modules as module}
-			<li>{module.name}@{module.version} {module.license ? `(${module.license})` : ''}</li>
-		{/each}
-	</ul>
+	<p class="mb-4 text-sm">生成日時: {data.generatedAt}</p>
+	{#each data.modules as module}
+		<article class="mb-2 rounded-2xl bg-gray-200 p-4">
+			<h2 class="text-lg font-semibold">{module.name}</h2>
+			<p class="mt-1 text-sm">
+				バージョン:{module.version}<br />
+				ライセンス:{module.license ? `(${module.license})` : ''}<br />
+			</p>
+		</article>
+	{/each}
+	<article class="mb-2 rounded-2xl bg-gray-200 p-4">
+		<h2 class="text-lg font-semibold">Font Awesome Free</h2>
+		<p class="mt-1 text-sm">
+			バージョン:null<br />
+			ライセンス:Icons - CC BY 4.0 License / Fonts - SIL OFL 1.1 License / Code - MIT License<br />
+		</p>
+	</article>
+	<article class="mb-2 rounded-2xl bg-gray-200 p-4">
+		<h2 class="text-lg font-semibold">CSS Stock</h2>
+		<p class="mt-1 text-sm">
+			バージョン:null<br />
+			ライセンス:独自ライセンス（商用利用可）<br />
+		</p>
+	</article>
+	<article class="mb-2 rounded-2xl bg-gray-200 p-4">
+		<h2 class="text-lg font-semibold">ICOON MONO</h2>
+		<p class="mt-1 text-sm">
+			バージョン:null<br />
+			ライセンス:独自ライセンス（商用利用可）<br />
+		</p>
+	</article>
+	<article class="mb-2 rounded-2xl bg-gray-200 p-4">
+		<h2 class="text-lg font-semibold">iconmonstr</h2>
+		<p class="mt-1 text-sm">
+			バージョン:null<br />
+			ライセンス:独自ライセンス（商用利用可）<br />
+		</p>
+	</article>
+	<article class="mb-2 rounded-2xl bg-gray-200 p-4">
+		<h2 class="text-lg font-semibold">シルエットイラスト</h2>
+		<p class="mt-1 text-sm">
+			バージョン:null<br />
+			ライセンス:独自ライセンス（商用利用可）<br />
+		</p>
+	</article>
+	<article class="mb-2 rounded-2xl bg-gray-200 p-4">
+		<h2 class="text-lg font-semibold">Misskey ロゴ</h2>
+		<p class="mt-1 text-sm">
+			バージョン:null<br />
+			ライセンス:CC BY-SA<br />
+		</p>
+	</article>
 </div>
