debfca1f6996816580dfbf7997b1f9ce58707ec6
taoki <84th.tccaoki@gmail.com>
2026-06-17 23:26:30 +0900

    add: 404回避のため内容はないが構成ページを追加

---
+++ src/routes/site/composition/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+	const { data } = $props();
+
+	let pageTitle = '構成';
+</script>
+
+<svelte:head>
+	<title>{pageTitle} | {data.site_title}</title>
+	<meta property="og:title" content="{pageTitle} | {data.site_title}" />
+</svelte:head>
+
+<div class="mb-6">
+	<p class="mb-4 text-xl">
+		<i class="fa-solid fa-circle-info"></i>
+		{pageTitle}
+	</p>
+</div>
+
+<div class="mt-4">
+	<p>準備中です。</p>
+</div>
