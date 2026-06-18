bfef7af7f9b443208cc2433caba6bd69cb14b286
taoki <84th.tccaoki@gmail.com>
2026-06-18 15:28:12 +0900

    feat: 記事カードをモバイル表示に最適化とヘッダーの検索ボタンのスタイルを修正

--- src/lib/components/PostCard.svelte
+++ src/lib/components/PostCard.svelte
@@ -8,12 +8,16 @@
 	{#each posts as post}
 		<a href={`/article/${post.category}/${post.slug}`}>
 			<article
-				class="article-card mb-4 flex gap-5 rounded-t-xs border-b-2 border-b-(--main-text-color) p-4"
+				class="article-card mb-4 flex gap-5 rounded-t-xs border-b-2 border-b-(--main-text-color) p-1 md:p-4"
 			>
-				<img src={post.thumbnail} alt="{post.title}のサムネイル" class="w-30 rounded-lg" />
+				<img
+					src={post.thumbnail}
+					alt="{post.title}のサムネイル"
+					class="h-24 w-30 shrink-0 rounded-lg object-cover"
+				/>
 				<div class="flex-1">
-					<h2 class="mb-2 text-2xl">{post.title}</h2>
-					<p>{post.heading}</p>
+					<h2 class="text-bace mb-2 md:text-2xl">{post.title}</h2>
+					<!--<p>{post.heading}</p>-->
 					<div class="flex">
 						<div class="ml-auto w-fit rounded-2xl border border-gray-500 px-2 py-1 text-right">
 							<p class="text-xs"><i class="fa-solid fa-folder-open mr-1"></i>{post.category}</p>
